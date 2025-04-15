50f77add92bb354ee1fb5267ef7bae3406a40cbd
Thebx1994 <thebx1994@gmail.com>
2025-04-14 23:14:26 -0400

    Save my local changes before merge

--- medicaltutordjproject/medicaltutordjproject/settings.py
+++ medicaltutordjproject/medicaltutordjproject/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
